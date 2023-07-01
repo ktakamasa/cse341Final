997ece41e86483f5473f37f2a3dd353ed2c09a48
ktakamasa <kohtakamasa@gmail.com>
2023-07-01 22:10:26 +0100

    start project

---
+++ server.js
@@ -0,0 +1,33 @@
+const express = require('express');
+const bodyParser = require('body-parser');
+const mongodb = require('./db/connect');
+
+const app = express();
+const port = process.env.PORT || 3000;
+
+app.use(bodyParser.json());
+app.use((req, res, next) => {
+  res.setHeader('Access-Control-Allow-Origin', '*');
+  next();
+});
+app.use('/', require('./routes'));
+
+// Error handling
+process.on('uncaughtException', (err, origin) => {
+  console.log(
+    process.stderr.fd,
+    `Caught exception: ${err}\n` + `Exception origin: ${origin}`
+  );
+});
+
+// mongodb.initDb((err) => {
+//   if (err) {
+//     console.log(err);
+//   } else {
+//     app.listen(port);
+//     console.log('Connected to DB and listening on ' + port);
+//   }
+// });
+
+app.listen(port);
+console.log('Listening on ' + port);
